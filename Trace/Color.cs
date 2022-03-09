61c8c2abfa55a0d7d0fe69f758ae328ddbeb31ff
Matteo Macchini <matteomacchini98@gmail.com>
2022-03-09 12:49:54 +0100

    Struct Color: example

--- Trace/Color.cs
+++ Trace/Color.cs
@@ -1,5 +1,14 @@
 ﻿namespace Trace;
-public class Class1
+
+public struct Color
 {
+    public float r, g, b;
+
+    public Color()
+    {
+        this.r = 0.;
+        this.g = 0.;
+        this.b = 0.;
+    }
 
 }
