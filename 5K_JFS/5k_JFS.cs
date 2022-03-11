284765e7294a49663fdcb952899fd3214dbca334
Gabriele Crespi <gabrielecrespi95@gmail.com>
2022-03-11 17:04:00 +0100

    frank

--- 5K_JFS/5k_JFS.cs
+++ 5K_JFS/5k_JFS.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 <<<<<<< HEAD
-Console.WriteLine("Hello, World!!");
+Console.WriteLine("Hello, World!");
     =======
 Console.WriteLine("Hello, World!!");
 >>>>>>> d1cc3fbc822c8c1a18bfba98a256bb1ec28ea9cb
