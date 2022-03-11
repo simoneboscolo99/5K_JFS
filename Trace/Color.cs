be926aae878728add254a36e356cdf37a01ed68a
Gabriele Crespi <gabrielecrespi95@gmail.com>
2022-03-11 17:00:45 +0100

    Frank commit

--- Trace/Color.cs
+++ Trace/Color.cs
@@ -6,9 +6,9 @@ public struct Color
 
     public Color()
     {
-        this.r = 0.;
-        this.g = 0.;
-        this.b = 0.;
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
     }
-
+    
 }
