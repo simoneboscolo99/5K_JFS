6504002f237454164327b8fff4f44d969a4e690c
Matteo Macchini <matteomacchini98@gmail.com>
2022-03-15 16:53:13 +0100

    Few Changes in Functions, Color Tests, Class HdrImage

--- Trace/Color.cs
+++ Trace/Color.cs
@@ -29,6 +29,7 @@ public struct Color
         => new Color(a.R * b.R, a.G * b.G, a.B * b.B);
     
     public override string ToString() => $"({R}, {G}, {B})";
+    
 	public static bool Is_Close (Color b)
-		=> Are_Close(this.R, b.R) && Are_Close(this.G, b.G) && Are_Close(this.B , b.B);
+		=> Functions.Are_Close(this.R, b.R) && Functions.Are_Close(this.G, b.G) && Functions.Are_Close(this.B , b.B);
 }
--- Trace/Functions.cs
+++ Trace/Functions.cs
@@ -1,11 +1,11 @@
 namespace Trace;
-{
-    
-}
 
 public class Functions
 {
-    public static bool Are_Close (float value1, float value2);
-        =>Math.Abs(value1 - value2) < 1^(-5);
+    public static bool Are_Close(float value1, float value2)
+    {
+        float eps = 1e-5f;
+        return Math.Abs(value1 - value2) < eps;
+    }
 
 }
