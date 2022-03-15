40c32c78476c66d416beaea2950d1f0997173a8c
Simone Boscolo <simoneboscolo15@gmail.com>
2022-03-15 16:11:11 +0100

    Implementato Metodo Is_Close

--- Trace/Color.cs
+++ Trace/Color.cs
@@ -8,13 +8,13 @@ public struct Color
     public float G { get; set; }
     public float B { get; set; }
 
-    public Color (float r, float g, float b)
+    public Color(float r, float g, float b)
     {
         R = r;
         G = g;
         B = b;
     }
-    
+
     public static Color operator +(Color a, Color b)
         => new Color(a.R + b.R, a.G + b.G, a.B + b.B);
     public static Color operator -(Color a, Color b)
@@ -24,5 +24,6 @@ public struct Color
     public static Color operator *(Color a, Color b)
         => new Color(a.R * b.R, a.G * b.G, a.B * b.B);
     public override string ToString() => $"({R}, {G}, {B})";
-
+	public static bool Is_Close (Color b)
+		=> Are_Close(this.R, b.R) && Are_Close(this.G, b.G) && Are_Close(this.B , b.B);
 }
