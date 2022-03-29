72e22c63e8ae7fdb5c451274d57f183c51bd2c6d
Matteo Macchini <matteomacchini98@gmail.com>
2022-03-29 17:51:05 +0200

    Finally a main, but some bugs

--- 5K_JFS/5k_JFS.cs
+++ 5K_JFS/5k_JFS.cs
@@ -1,19 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using _5K_JFS;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 using Trace;
 
+try
+{
+    Parameters.Parse_Command_Line(args);
+    Console.WriteLine(Parameters.InputPfmFileName);
+    Console.WriteLine(Parameters.A);
+    Console.WriteLine(Parameters.Gamma);
+    Console.WriteLine(Parameters.OutputPngFileName);
+    
+    HdrImage image = new HdrImage(Parameters.InputPfmFileName);
 
-var image = new HdrImage("memorial.pfm");
-// Create a sRGB bitmap
-var bitmap = new Image<Rgb24>(Configuration.Default, image.Width, image.Height);
+    image.Luminosity_Norm(Parameters.A);
+    image.Clamp_Image();
+    // Create a sRGB bitmap
+    //var bitmap = new Image<Rgb24>(Configuration.Default, image.Width, image.Height);
 
 // Save the bitmap as a PNG file
-using (Stream fileStream = File.OpenWrite("output.png")) {
-    bitmap.Save(fileStream, new PngEncoder());
+   using (Stream fileStream = File.OpenWrite("output.pfm")) {
+   //   bitmap.Save(fileStream, new PngEncoder());
+    image.Write_Ldr_Image(fileStream, "png", Parameters.Gamma);
+   }
 }
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
 Console.WriteLine("Hello, World!");
 
 
