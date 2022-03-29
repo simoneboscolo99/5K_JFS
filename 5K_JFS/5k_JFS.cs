a3095a80859b1b0d39994da5237eefd699d21a51
Gabriele Crespi <gabrielecrespi95@gmail.com>
2022-03-29 15:31:48 +0200

    Status

--- 5K_JFS/5k_JFS.cs
+++ 5K_JFS/5k_JFS.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
 using Trace;
 
+
+var image = new HdrImage("memorial.pfm");
+// Create a sRGB bitmap
+var bitmap = new Image<Rgb24>(Configuration.Default, image.Width, image.Height);
+
+// Save the bitmap as a PNG file
+using (Stream fileStream = File.OpenWrite("output.png")) {
+    bitmap.Save(fileStream, new PngEncoder());
+}
 Console.WriteLine("Hello, World!");
+
+
 
