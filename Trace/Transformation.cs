0dfa35732bfda1dd9e71b6619c9c2acd325fb4c7
Simone Boscolo <simoneboscolo15@gmail.com>
2022-04-06 11:32:46 +0200

    Translation

--- Trace/Transformation.cs
+++ Trace/Transformation.cs
@@ -6,4 +6,11 @@ public struct Transformation
 {
     public Matrix4x4 M = new Matrix4x4(); 
     public Matrix4x4 InvM = new Matrix4x4();
+
+    public Transformation Translation(Vec v)
+    {
+        M = (1.0f, 0.0f, 0.0f, v.X, 0.0f, 1.0f, 0.0f, v.Y, 0.0f, 0.0f, 1.0f, v.Z, 0.0f, 0.0f, 0.0f, 1.0f);
+        InvM = (1.0f, 0.0f, 0.0f, -v.X, 0.0f, 1.0f, 0.0f, -v.Y, 0.0f, 0.0f, 1.0f, -v.Z, 0.0f, 0.0f, 0.0f, 1.0f);
+    }
+    
 }
