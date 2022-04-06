17dd5556307b4ebd7b35c54b748e5379e6526a0c
Matteo Macchini <matteomacchini98@gmail.com>
2022-04-06 11:31:54 +0200

    Transformation: constructor and IsConsistent

--- Trace/Transformation.cs
+++ Trace/Transformation.cs
@@ -4,6 +4,20 @@ namespace Trace;
 
 public struct Transformation
 {
-    public Matrix4x4 M = new Matrix4x4(); 
-    public Matrix4x4 InvM = new Matrix4x4();
+    public Matrix4x4 M; //= new Matrix4x4(); 
+    public Matrix4x4 InvM; // = new Matrix4x4();
+    
+    public Transformation(Matrix4x4 m, Matrix4x4 invM)
+    {
+        //var m1 = m ?? Matrix4x4.Identity;
+        M = m;
+        InvM = invM;
+    }
+
+    public bool Is_Consistent()
+    {
+        var I = Matrix4x4.Multiply(M, InvM);
+        return I.IsIdentity;
+    }
+    
 }
