d7e1642fe1bdbf10796a014f30e70c3613bbda49
Simone Boscolo <simoneboscolo15@gmail.com>
2022-04-06 10:23:46 +0200

    I've added two new PointTests

--- Trace.Tests/PointTests.cs
+++ Trace.Tests/PointTests.cs
@@ -7,6 +7,7 @@ public class PointTests
     Point a = new(1.0f, 2.0f, 3.0f);
     Point b = new(4.0f, 6.0f, 8.0f);
     float scalar = 2.0f;
+    private Vec c = new(4.0f, 6.0f, 8.0f);
 
     [Fact]
     public void TestPoints()
@@ -26,6 +27,10 @@ public class PointTests
         Assert.False((a + b).Is_Close(new Point(5.0f, 9.0f, 11.0f)), "Point+Point does not work");
         Assert.True((b - a).Is_Close(new Vec(3.0f, 4.0f, 5.0f)), "Test Point-Point");
         Assert.False((b - a).Is_Close(new Vec(3.0f, 4.0f, 6.0f)), "Point-Point does not work");
+        Assert.True((a + c).Is_Close(new Point(5.0f, 8.0f, 11.0f)), "Test Point+Vec");
+        Assert.False((a + c).Is_Close(new Point(5.0f, 10.0f, 11.0f)), "Point+Vec does not work");
+        Assert.True((a - c).Is_Close(new Point(-3.0f, -4.0f, -5.0f)), "Test Point-Vec");
+        Assert.False((a - c).Is_Close(new Point(3.0f, -4.0f, -5.0f)), "Point-Vec does not work");
     }
     
 }
