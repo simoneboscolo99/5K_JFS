0285677bf6c854d0297025839f3397e95496b3cf
Gabriele Crespi <gabrielecrespi95@gmail.com>
2022-04-13 17:47:32 +0200

    Camera class documentation

--- Trace.Tests/CamerasTests.cs
+++ Trace.Tests/CamerasTests.cs
@@ -1,6 +1,19 @@
+using Xunit;
+
 namespace Trace.Tests;
 
 public class CamerasTests
 {
+
+    [Fact]
+    public void Is_Close_Test()
+    {
+    }
+
+    [Fact]
+    public void At_Test()
+    {
+
+    }
 
 }
