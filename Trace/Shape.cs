228e501237160343516fbb19425cc18b9770e25e
Simone Boscolo <simoneboscolo15@gmail.com>
2022-05-04 11:33:50 +0200

    Commit 2

--- Trace/Shape.cs
+++ Trace/Shape.cs
@@ -1,6 +1,13 @@
 namespace Trace;
 
-public class Shape
+public abstract class Shape
 {
-    
+    public Transformation Tr { get; set; }
+
+    public void Init(Transformation t)
+    {
+        Tr = t;
+    }
+
+    public abstract HitRecord? Ray_Intersection(Ray r);
 }
--- Trace/Sphere.cs
+++ Trace/Sphere.cs
@@ -1,6 +1,16 @@
 namespace Trace;
 
-public class Sphere
+public class Sphere: Shape
 {
-    
+    public override HitRecord? Ray_Intersection(Ray r)
+    {
+        throw new NotImplementedException("Shape.Ray_Intersection is an abstract method and cannot be called directly");
+    }
+
+    public Vec2D Sphere_Point_to_uv(Point p)
+    {
+        var u = Math.Atan2(p.Y, p.X) / (2.0 * Math.PI);
+        var vec = Vec2D(3, Math.Acos(Math.Cos((p.Z)/Math.PI)));
+        return vec;
+    }
 }
