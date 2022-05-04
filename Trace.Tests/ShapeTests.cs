6c353194fe0f7b97b0fead488ff00c8bb683dd27
Gabriele Crespi <gabrielecrespi95@gmail.com>
2022-05-04 12:51:36 +0200

    Test failed

--- Trace.Tests/ShapeTests.cs
+++ Trace.Tests/ShapeTests.cs
@@ -1,6 +1,21 @@
+using Xunit;
+
 namespace Trace.Tests;
 
-public class ShapeTests
+public class SphereTests
 {
-    
+
+    [Fact]
+    public void Test_Ray_Intersection()
+    {
+        var sph = new Sphere();
+        var ray1 = new Ray(new Point(0.0f, 0.0f, 2.0f), new Vec(0.0f, 0.0f, -1.0f));
+        var inter = sph.Ray_Intersection(ray1);
+
+        Assert.True(new HitRecord(new Point(0.0f, 0.0f, 1.0f), new Normal(0.0f, 0.0f, 1.0f), 1.0f, ray1,
+            new Vec2D(0.0f, 0.0f)).Is_Close(inter));
+
+    }
+
+
 }
