fa3254814fd29f9bbc8427a318ba339a7dd992c4
Matteo Macchini <matteomacchini98@gmail.com>
2022-05-11 12:27:55 +0200

    Merge master in pathtracing. Some classes were missing

--- Trace/Pcg.cs
+++ Trace/Pcg.cs
@@ -1,5 +1,18 @@
 namespace Trace;
 
+// ====== INT TYPES C# ====== INT TYPES C# ====== INT TYPES C# ====== INT TYPES C# ======
+// - byte - 8-bit unsigned integer - 0 to 255
+// - sbyte - 8-bit signed integer - -128 to 127
+// - short - 16-bit signed integer - -32,768 to 32,767
+// - ushort - 16-bit unsigned integer - 0 to 65,535
+// - int - 32-bit signed integer - -2,147,483,648 to 2,147,483,647
+// - uint - 32-bit unsigned integer - 0 to 4,294,967,295
+// - long - 64-bit signed integer - -9,223,372,036,854,775,808 to9,223,372,036,854,775,807
+// - ulong - 64-bit unsigned integer - 0 to 18,446,744,073,709,551,615
+
+/// <summary>
+/// 
+/// </summary>
 public class Pcg
 {
     
