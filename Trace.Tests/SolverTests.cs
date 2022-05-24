68c840829542458d4dcb9337bcee30be7dfab609
Gabriele Crespi <gabrielecrespi95@gmail.com>
2022-05-24 11:47:21 +0200

    PathTracing Tests

--- Trace.Tests/SolverTests.cs
+++ Trace.Tests/SolverTests.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Xunit;
 
 namespace Trace.Tests;
@@ -57,6 +58,34 @@ public class SolverTests
         Assert.True(image.Get_Pixel(0, 2).Is_Close(Color.Black),"Test 7");
         Assert.True(image.Get_Pixel(1, 2).Is_Close(Color.Black), "Test 8");
         Assert.True(image.Get_Pixel(2, 2).Is_Close(Color.Black), "Test 9");
+    }
+
+    [Fact]
+    public void Furnace_Test()
+    {
+        var pcg = new Pcg();
+        //Run the furnace_test several times using random values for the emitted radiance and reflectance
+
+        for (int i = 0; i < 7; i++)
+        {
+            var world = new World();
+            var emittedRadiance = pcg.Random_Float();
+            var reflectance = pcg.Random_Float() * 0.9f; //Be sure to pick a reflectance not too close to 1
+
+            var sphere = new Sphere(null,new Material(new DiffuseBrdf(new UniformPigment(Color.White * reflectance)),
+                new UniformPigment(Color.White * emittedRadiance)));
+            world.Add(sphere);
+            var pathTracer = new PathTracing(world, null, pcg,1, 100, 101);
+            var ray = new Ray(new Point(), new Vec(1.0f, 0.0f, 0.0f));
+            var color = pathTracer.Tracing(ray);
+
+            var expected = emittedRadiance / (1.0f - reflectance);
+            
+            Assert.True(Functions.Are_Close(expected, color.R));
+            Assert.True(Functions.Are_Close(expected, color.G));
+            Assert.True(Functions.Are_Close(expected, color.B));
+
+        }
     }
     
 }
