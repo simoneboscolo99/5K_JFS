732989f98dffb477a069d3e750dc681de0e7310f
Matteo Macchini <matteomacchini98@gmail.com>
2022-05-25 11:33:58 +0200

    GrammarErrorException created

--- Trace/Materials.cs
+++ Trace/Materials.cs
@@ -66,41 +66,40 @@ public class CheckeredPigment : Pigment
       false => C2
     };
   }
-
-  /// <summary>
-  /// A textured pigment
-  /// The texture is given through a PFM image.
-  /// </summary>
-  public class ImagePigment : Pigment
-  {
-    //texture given by an image
-    public HdrImage Img;
-
-    public ImagePigment(HdrImage img)
-    {
-      Img = img;
-    }
-
-    public override Color Get_Color(Vec2D v)
-    {
-      //"casting" (u,v) coordinates on image points
-      var col = (int) (v.U * Img.Width);
-      var row = (int) (v.V * Img.Height);
-
-      if (col >= Img.Width)
-        col = Img.Width - 1;
-
-      if (row >= Img.Height)
-        row = Img.Height - 1;
-
-      /* A nicer solution would implement bilinear interpolation to reduce pixelization artifacts
-      See https://en.wikipedia.org/wiki/Bilinear_interpolation */ //finita brdf lo faccio
-      return Img.Get_Pixel(col, row);
-    }
-    
-  }
-  
 }
+
+/// <summary>
+/// A textured pigment
+/// The texture is given through a PFM image.
+/// </summary>
+public class ImagePigment : Pigment
+{
+  //texture given by an image
+  public HdrImage Img;
+
+  public ImagePigment(HdrImage img)
+  {
+    Img = img;
+  }
+
+  public override Color Get_Color(Vec2D v)
+  {
+    //"casting" (u,v) coordinates on image points
+    var col = (int) (v.U * Img.Width);
+    var row = (int) (v.V * Img.Height);
+
+    if (col >= Img.Width)
+      col = Img.Width - 1;
+
+    if (row >= Img.Height)
+      row = Img.Height - 1;
+
+    /* A nicer solution would implement bilinear interpolation to reduce pixelization artifacts
+    See https://en.wikipedia.org/wiki/Bilinear_interpolation */ //finita brdf lo faccio
+    return Img.Get_Pixel(col, row);
+  }
+}
+
 
 /// <summary>
 /// An abstract class representing a Bidirectional Reflectance Distribution Function
